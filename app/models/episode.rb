8a8a4c324de63bae256883ca3ed048f0200a090d
HolyCow <mike.beasterfeld@gmail.com>
2014-02-09 21:31:48 -0600

    update show page to include episode number, part number, and episode title

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,15 @@ class Episode < ActiveRecord::Base
 	has_many :users, :through => :episodehosts
 
 	belongs_to :show
+
+	def full_title
+		full_title = self.number.to_s
+
+		full_title << " Part #{self.part}" if !self.part.nil?
+
+		full_title << ": #{self.title}"
+
+		return full_title
+	end
+
 end
