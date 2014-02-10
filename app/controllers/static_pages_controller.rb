e28bce0e54c4cca1a0938b1509b3355e065ce0a0
HolyCow <mike.beasterfeld@gmail.com>
2014-02-09 21:16:47 -0600

    remove depreciated find(:all)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
 	def index
 		@shows = Show.all
-		@episodes = Episode.find(:all, :order => "id desc", :limit => 5)
+		@episodes = Episode.order("id desc").limit(5)
 	end
 
 end
