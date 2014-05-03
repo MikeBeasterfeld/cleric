9815a0222c5b0ad60c20dd98c24afa5be4b21239
HolyCow <mike.beasterfeld@gmail.com>
2014-05-03 00:23:19 -0500

    remove rails admin reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,4 @@ class User < ActiveRecord::Base
 
   has_many :episodeguests
   has_many :guestepisodes, :through => :episodeguests, :source => :user
-
-	rails_admin do
-    configure :showhosts do
-      visible(false)
-    end
-
-    configure :shows do
-      orderable(true) # only for multiselect widget currently. Will add the possibility to order blocks
-      # configuration here
-    end
-  end	
 end
