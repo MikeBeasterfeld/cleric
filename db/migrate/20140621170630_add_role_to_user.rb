20ead441a6d4718a63ddc4e3451f9a2a6aa813c9
HolyCow <mike.beasterfeld@gmail.com>
2014-06-21 14:11:18 -0500

    fix user type migration

--- db/migrate/20140621170630_add_role_to_user.rb
+++ db/migrate/20140621170630_add_role_to_user.rb
@@ -2,7 +2,7 @@ class AddRoleToUser < ActiveRecord::Migration
   def change
     add_column :users, :role, :string
 
-    User.each do |user|
+    User.all.each do |user|
       user.role = 'admin' if user.admin?
     end
   end
