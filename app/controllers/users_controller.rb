8b9e26756f806066790804b64a4405851e6cd55a
HolyCow <mike.beasterfeld@gmail.com>
2014-06-22 01:04:33 -0500

    Don't allow anyone but admins change a users role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,8 @@ class UsersController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :name, :bio, :twitter, :password, :password_confirmation, :email, :role)
+      permitted = [:username, :name, :bio, :twitter, :password, :password_confirmation, :email]
+      permitted << :role if current_user.admin?
+      params.require(:user).permit(permitted)
     end
 end
