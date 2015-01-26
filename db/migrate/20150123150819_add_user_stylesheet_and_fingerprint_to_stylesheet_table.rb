52c3611e15f71b221337de5d1ee7ec6fb3a92203
HolyCow <mike.beasterfeld@gmail.com>
2015-01-25 21:23:28 -0600

    create initial stylesheet if it does not exist

--- db/migrate/20150123150819_add_user_stylesheet_and_fingerprint_to_stylesheet_table.rb
+++ db/migrate/20150123150819_add_user_stylesheet_and_fingerprint_to_stylesheet_table.rb
@@ -1,6 +1,8 @@
 class AddUserStylesheetAndFingerprintToStylesheetTable < ActiveRecord::Migration
   def change
     add_column :stylesheets, :user_stylesheet, :text    
-    add_column :stylesheets, :fingerprint, :string    
+    add_column :stylesheets, :fingerprint, :string
+
+    Stylesheet.create if Stylesheet.count == 0
   end
 end
