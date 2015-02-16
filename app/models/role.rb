3b16da71f0e1b817e59d408224f367bb3d6d6e0d
HolyCow <mike.beasterfeld@gmail.com>
2015-02-16 10:46:13 -0600

    make user role names unique

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,8 @@
 class Role < ActiveRecord::Base
   ROLES = %w[admin templates users shows episodes]
 
+  validates_uniqueness_of :name
+
   has_many :userroles
   has_many :users, :through => :userroles
 end
